f97e089c2b1d99bf565fef44a41585885f463955
csouza <ccs1201@gmail.com>
2024-03-10 12:50:35 -0300

    #Aula 2.11 Update objeto

--- src/test/java/br/com/ccs/ejpa/repositories/OperacoesTransacionadasTest.java
+++ src/test/java/br/com/ccs/ejpa/repositories/OperacoesTransacionadasTest.java
@@ -3,6 +3,8 @@ package br.com.ccs.ejpa.repositories;
 import br.com.ccs.ejpa.entities.Produto;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class OperacoesTransacionadasTest extends BaseRepositotyTest {
@@ -26,7 +28,7 @@ public class OperacoesTransacionadasTest extends BaseRepositotyTest {
 
     @Test
     void removerObjeto() {
-        var p = em.find(Produto.class, 3);
+        var p = em.find(Produto.class, 1);
         begin();
         em.remove(p);
         commit();
@@ -34,5 +36,30 @@ public class OperacoesTransacionadasTest extends BaseRepositotyTest {
         p = em.find(Produto.class, p.getId());
 
         assertNull(p);
+    }
+
+    @Test
+    void updateObjeto() {
+
+        var p = em.find(Produto.class, 2);
+        p.setNome("teste");
+        p.setDescricao("descricao");
+        p.setPreco(BigDecimal.valueOf(10.00).setScale(2));
+
+        begin();
+        em.merge(p);
+        commit();
+
+        em.clear();
+
+        var actual = em.find(Produto.class, p.getId());
+
+        assertNotNull(p);
+
+        assertEquals(p.getId(), actual.getId());
+        assertEquals(p.getNome(), actual.getNome());
+        assertEquals(p.getDescricao(), actual.getDescricao());
+        assertEquals(p.getPreco(), actual.getPreco());
+
     }
 }
